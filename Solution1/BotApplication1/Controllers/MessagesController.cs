719d280e8bfe6d56f614c7f4fac636249ce0b887
IngyunAhn <goltong1@gmail.com>
2018-10-15 18:54:02 +0900

    no message

--- Solution1/BotApplication1/Controllers/MessagesController.cs
+++ Solution1/BotApplication1/Controllers/MessagesController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -20,7 +18,7 @@ namespace BotApplication1
         {
             if (activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.KaKaoDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
             }
             else
             {
@@ -40,12 +38,9 @@ namespace BotApplication1
             }
             else if (messageType == ActivityTypes.ConversationUpdate)
             {
-                if (message.MembersAdded.Any(o => o.Id == message.Recipient.Id))
-                {
-                    //var reply = message.CreateReply("안녕하십니까. 저는 학생용 도우미 챗봇 Artoria입니다. 서비스를 시작하시려면 아무 말씀이나 해주십시오.");
-                    //ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
-                    //connector.Conversations.ReplyToActivityAsync(reply);
-                }
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
             }
             else if (messageType == ActivityTypes.ContactRelationUpdate)
             {
@@ -62,10 +57,5 @@ namespace BotApplication1
 
             return null;
         }
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
-
     }
 }
--- Solution1/BotApplication1/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BotApplication1.WebApiApplication" Language="C#" %>
